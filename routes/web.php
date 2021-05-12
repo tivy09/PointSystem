1c5baccde934bebb12b1475ed9e5cd58e69369c6
JianHui1208 <jianhuiwilliam80@gmail.com>
2021-05-12 23:50:52 +0800

    Change Route Typo

--- routes/web.php
+++ routes/web.php
@@ -136,7 +136,7 @@ Route::get('/Project/List',[                'uses'=>'ProjectController@indexProj
 Route::post('/Project/List/Enroll',[        'uses'=>'ProjectController@enrollProjectList',       'as'=>'admin.Project.enrollProjectList']);
 Route::get('/Project/Evaluation',[          'uses'=>'ProjectController@indexProjectEvaluation',  'as'=>'admin.Project.Evaluation']);
 Route::get('/Project/Evaluation/Record/{id}',[          'uses'=>'ProjectController@ProjectEvaluationRecord',  'as'=>'admin.Project.EvaluationRecord']);
-Route::post('/Project/Evaluation/store',[              'uses'=>'ProjectController@d',            'as'=>'admin.Project.storeProjectEvaluation']);
+Route::post('/Project/Evaluation/store',[              'uses'=>'ProjectController@storeProjectEvaluation',            'as'=>'admin.Project.storeProjectEvaluation']);
 
 //face
 Route::get('/Home/Photo/Check',[    'uses'=>'AvaterController@index',     'as'=>'Avatar.index']);
